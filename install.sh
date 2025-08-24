2113f7381303a9209bec84e9410b9c8dbab65b4c
nicholasrupert <nersups@proton.me>
2025-08-23 19:24:24 -0500

    minor documentation edits

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cp ./snail.pl /usr/local/bin/snail
 chmod +x /usr/local/bin/snail
 mkdir ~/.config
 cp ./.snailrc ~/.config/snailrc
-cp ./snailrc.example ~/.config/snailrc.example
+cp ./.snailrc.example ~/.config/snailrc.example
